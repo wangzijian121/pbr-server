c9ceef223b6536c36b97e68cf118d295ce1352d6
wangzijian <1219116968@qq.com>
2023-07-26 13:32:50 +0800

    BUG : Mybatis PlusInterceptor #5

--- pose-management-dao/src/main/java/com/zlht/pose/management/dao/DaoConfiguration.java
+++ pose-management-dao/src/main/java/com/zlht/pose/management/dao/DaoConfiguration.java
@@ -20,12 +20,24 @@
 
 package com.zlht.pose.management.dao;
 
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
 @MapperScan("com.zlht.pose.management.dao.mapper")
 public class DaoConfiguration {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        return interceptor;
+    }
+
 }
