f22a4a6bc50349607826635394795af122d5e29d
wangzijian <1219116968@qq.com>
2023-08-24 23:47:15 +0800

    update: update bin/stop.sh  #44

--- bin/stop.sh
+++ bin/stop.sh
@@ -8,18 +8,6 @@ if [ -z "$PID" ]; then
 fi
 
 echo "Stopping AlgorithmManagementApi process $PID"
-kill -SIGTERM $PID
+kill $PID
 
-# Wait for up to 30 seconds for the process to stop
-for i in {1..30}; do
-    if ps -p $PID > /dev/null; then
-        sleep 1
-    else
-        echo "AlgorithmManagementApi process $PID stopped"
-        exit 0
-    fi
-done
-
-echo "AlgorithmManagementApi process $PID did not stop in a timely manner, sending SIGKILL"
-kill -SIGKILL $PID
 exit 0
